a6d6a63bbdfadedd64a71d3a4cce2e640502810b
RavinaB <ravina.bhayani8@gmail.com>
2026-03-21 13:32:46 -0400

    First Commit

---
+++ HappyBirthday/app/build.gradle.kts
@@ -0,0 +1,52 @@
+    plugins {
+        alias(libs.plugins.android.application)
+        id("org.jetbrains.kotlin.android")
+    }
+
+    android {
+        namespace = "com.example.happybirthday"
+        compileSdk = 34
+
+        defaultConfig {
+            applicationId = "com.example.happybirthday"
+            minSdk = 24
+            targetSdk = 34
+            versionCode = 1
+            versionName = "1.0"
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+
+        composeOptions {
+            kotlinCompilerExtensionVersion = "1.5.8"
+        }
+
+        buildFeatures {
+            compose = true
+        }
+    }
+    kotlin {
+        jvmToolchain(11)
+    }
+    dependencies {
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.lifecycle.runtime.ktx)
+        implementation(libs.androidx.activity.compose)
+        implementation(platform(libs.androidx.compose.bom))
+        implementation(libs.androidx.compose.ui)
+        implementation(libs.androidx.compose.ui.graphics)
+        implementation(libs.androidx.compose.ui.tooling.preview)
+        implementation(libs.androidx.compose.material3)
+        implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+//        implementation(libs.androidx.ui)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+        androidTestImplementation(platform(libs.androidx.compose.bom))
+        androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+        debugImplementation(libs.androidx.compose.ui.tooling)
+        debugImplementation(libs.androidx.compose.ui.test.manifest)
+    }
