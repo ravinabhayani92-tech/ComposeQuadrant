a4214bf1d2b079e83e64b1e20406d3a88772c04c
RavinaB <ravina.bhayani8@gmail.com>
2026-03-21 20:32:46 -0400

    Upgrade gradle files

--- HappyBirthday/app/build.gradle.kts
+++ HappyBirthday/app/build.gradle.kts
@@ -1,51 +1,109 @@
-    plugins {
-        alias(libs.plugins.android.application)
-        id("org.jetbrains.kotlin.android")
-    }
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+}
 
-    android {
-        namespace = "com.example.happybirthday"
-        compileSdk = 34
-
-        defaultConfig {
-            applicationId = "com.example.happybirthday"
-            minSdk = 24
-            targetSdk = 34
-            versionCode = 1
-            versionName = "1.0"
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
-        }
-
-        composeOptions {
-            kotlinCompilerExtensionVersion = "1.5.8"
-        }
-
-        buildFeatures {
-            compose = true
+android {
+    namespace = "com.example.happybirthday"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
         }
     }
-    kotlin {
-        jvmToolchain(11)
+
+    defaultConfig {
+        applicationId = "com.example.happybirthday"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    dependencies {
-        implementation(libs.androidx.core.ktx)
-        implementation(libs.androidx.lifecycle.runtime.ktx)
-        implementation(libs.androidx.activity.compose)
-        implementation(platform(libs.androidx.compose.bom))
-        implementation(libs.androidx.compose.ui)
-        implementation(libs.androidx.compose.ui.graphics)
-        implementation(libs.androidx.compose.ui.tooling.preview)
-        implementation(libs.androidx.compose.material3)
-//        implementation(libs.androidx.ui)
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.androidx.junit)
-        androidTestImplementation(libs.androidx.espresso.core)
-        androidTestImplementation(platform(libs.androidx.compose.bom))
-        androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-        debugImplementation(libs.androidx.compose.ui.tooling)
-        debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
+//    plugins {
+//        alias(libs.plugins.android.application)
+//        id("org.jetbrains.kotlin.android")
+//    }
+//
+//    android {
+//        namespace = "com.example.happybirthday"
+//        compileSdk = 34
+//
+//        defaultConfig {
+//            applicationId = "com.example.happybirthday"
+//            minSdk = 24
+//            targetSdk = 34
+//            versionCode = 1
+//            versionName = "1.0"
+//        }
+//
+//        compileOptions {
+//            sourceCompatibility = JavaVersion.VERSION_11
+//            targetCompatibility = JavaVersion.VERSION_11
+//        }
+//
+//        composeOptions {
+//            kotlinCompilerExtensionVersion = "1.5.8"
+//        }
+//
+//        buildFeatures {
+//            compose = true
+//        }
+//    }
+//    kotlin {
+//        jvmToolchain(11)
+//    }
+//    dependencies {
+//        implementation(libs.androidx.core.ktx)
+//        implementation(libs.androidx.lifecycle.runtime.ktx)
+//        implementation(libs.androidx.activity.compose)
+//        implementation(platform(libs.androidx.compose.bom))
+//        implementation(libs.androidx.compose.ui)
+//        implementation(libs.androidx.compose.ui.graphics)
+//        implementation(libs.androidx.compose.ui.tooling.preview)
+//        implementation(libs.androidx.compose.material3)
+////        implementation(libs.androidx.ui)
+//        testImplementation(libs.junit)
+//        androidTestImplementation(libs.androidx.junit)
+//        androidTestImplementation(libs.androidx.espresso.core)
+//        androidTestImplementation(platform(libs.androidx.compose.bom))
+//        androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+//        debugImplementation(libs.androidx.compose.ui.tooling)
+//        debugImplementation(libs.androidx.compose.ui.test.manifest)
+//    }
