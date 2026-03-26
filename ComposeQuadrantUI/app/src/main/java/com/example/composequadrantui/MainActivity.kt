e1d87054c57b099c0c4cc2afcfa2a33f55c59aeb
RavinaB <ravina.bhayani8@gmail.com>
2026-03-25 22:07:36 -0400

    Remove unwanted code

--- ComposeQuadrantUI/app/src/main/java/com/example/composequadrantui/MainActivity.kt
+++ ComposeQuadrantUI/app/src/main/java/com/example/composequadrantui/MainActivity.kt
@@ -40,14 +40,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
 fun ComposeQuadrant(){
     Box{
         Column(modifier = Modifier.fillMaxSize()){
